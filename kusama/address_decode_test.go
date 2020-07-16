7703618a7dc813f1b1a61ddf9a7b4426bdafa6a1
GarFoonYellowLee <jiahuanyellowlee@163.com>
2020-07-16 23:52:41 +0800

    1.修复为支持ksm链 2.增加映射支持

---
+++ kusama/address_decode_test.go
@@ -0,0 +1,76 @@
+package kusama
+
+import (
+	"encoding/hex"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestAddressDecoder_AddressEncode(t *testing.T) {
+	result := "HcKJ5nYJoTXuKTKv96mdLbmS7MYwLZYRbUgLTXB4D8VRFLj"
+	p2pk, _ := hex.DecodeString("deb2e8c58b53dbe18cf9b103d06f8e3a83bff78443ba8750ecc1cb9a31532363")
+	p2pkAddr, _ := tw.Decoder.AddressEncode(p2pk)
+	fmt.Println("p2pkAddr: ", p2pkAddr, strings.EqualFold(result, p2pkAddr) )
+
+	result = "EG3yPrrBkKoeLa9edakAM74oNDTuQMWpTDyHQHHvJMJ9ZxK"
+	p2pk, _ = hex.DecodeString("4a89b8be1072ee1e71ea46430715fbfe8244c76512896980eb2adeed3518aa74")
+	p2pkAddr, _ = tw.Decoder.AddressEncode(p2pk)
+	fmt.Println("p2pkAddr: ", p2pkAddr, strings.EqualFold(result, p2pkAddr) )
+}
+
+func TestAddressDecoder_AddressDecode(t *testing.T) {
+	result := "deb2e8c58b53dbe18cf9b103d06f8e3a83bff78443ba8750ecc1cb9a31532363"
+	p2pkAddr := "HcKJ5nYJoTXuKTKv96mdLbmS7MYwLZYRbUgLTXB4D8VRFLj"
+	p2pk, _ := tw.Decoder.AddressDecode(p2pkAddr)
+	p2pkStr := hex.EncodeToString(p2pk)
+	fmt.Println("p2pkStr: ", p2pkStr, strings.EqualFold(result, p2pkStr) )
+
+	result = "4a89b8be1072ee1e71ea46430715fbfe8244c76512896980eb2adeed3518aa74"
+	p2pkAddr = "EG3yPrrBkKoeLa9edakAM74oNDTuQMWpTDyHQHHvJMJ9ZxK"
+	p2pk, _ = tw.Decoder.AddressDecode(p2pkAddr)
+	p2pkStr = hex.EncodeToString(p2pk)
+	fmt.Println("p2pkStr: ", p2pkStr, strings.EqualFold(result, p2pkStr) )
+}
+
+func TestAddressDecoder(t *testing.T) {
+	pubkey := "deb2e8c58b53dbe18cf9b103d06f8e3a83bff78443ba8750ecc1cb9a31532363"
+	p2pkAddr := "HcKJ5nYJoTXuKTKv96mdLbmS7MYwLZYRbUgLTXB4D8VRFLj"
+
+	testP2pkAddr, _ := tw.Decoder.AddressEncode( hex.DecodeString(pubkey) )
+	fmt.Println("testP2pkAddr: ", testP2pkAddr, strings.EqualFold(testP2pkAddr, p2pkAddr) )
+
+	p2pk, _ := tw.Decoder.AddressDecode(p2pkAddr)
+	p2pkStr := hex.EncodeToString(p2pk)
+	fmt.Println("p2pkStr: ", p2pkStr, strings.EqualFold(pubkey, p2pkStr) )
+}
+
+func Test_ed25519_AddressVerify_Valid(t *testing.T) {
+	addressArr := make([]string, 0)
+	addressArr = append(addressArr, "HcKJ5nYJoTXuKTKv96mdLbmS7MYwLZYRbUgLTXB4D8VRFLj")	//正确
+	addressArr = append(addressArr, "HcKJ5nYJoTXuKTKv96mdLbmS7MYwLZYRbUgLTXB4D8VRFLi")	//改了最后一位
+	addressArr = append(addressArr, "AcKJ5nYJoTXuKTKv96mdLbmS7MYwLZYRbUgLTXB4D8VRFLj")	//改了第一位
+	addressArr = append(addressArr, "HcKJ5nYJoTXuKTKv96mdLbmS7MYwLZ26SUgLTXB4D8VRFLj")	//改了中间
+
+	for i := 0; i < len(addressArr); i++ {
+		address := addressArr[i]
+		valid := tw.Decoder.AddressVerify(address)
+
+		fmt.Println(address, " isvalid : ", valid)
+	}
+}
+
+func Test_sr25519_AddressVerify_Valid(t *testing.T) {
+	addressArr := make([]string, 0)
+	addressArr = append(addressArr, "CreEvbSZbf7bJ6ymX6UKNWmwBxmyXK6j29QWQGVvtr7TLsn")	//正确
+	addressArr = append(addressArr, "CreEvbSZbf7bJ6ymX6UKNWmwBxmyXK6j29QWQGVvtr7TLs7")	//改了最后一位
+	addressArr = append(addressArr, "WreEvbSZbf7bJ6ymX6UKNWmwBxmyXK6j29QWQGVvtr7TLsn")	//改了第一位
+	addressArr = append(addressArr, "WreEvbSZbf7bJ6ymX6UKNWm1516yXK6j29QWQGVvtr7TLsn")	//改了中间
+
+	for i := 0; i < len(addressArr); i++ {
+		address := addressArr[i]
+		valid := tw.Decoder.AddressVerify(address)
+
+		fmt.Println(address, " isvalid : ", valid)
+	}
+}
--- kusama/kusama.go
+++ kusama/kusama.go
@@ -10,6 +10,7 @@ const (
 	MasterKey = "Kusama seed"
 	GenesisHash = "b0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe"
 	SpecVersion =  1058
+	AddrPrefix = 0x02
 )
 
 type WalletManager struct {
@@ -19,7 +20,7 @@ type WalletManager struct {
 func NewWalletManager() *WalletManager {
 	wm := WalletManager{}
 	wm.WalletManager = polkadot.NewWalletManager()
-	wm.Config = polkadot.NewConfig(Symbol, MasterKey, GenesisHash, SpecVersion)
+	wm.Config = polkadot.NewConfig(Symbol, MasterKey, GenesisHash, SpecVersion, AddrPrefix)
 	//wm.LoadAssetsConfig( wm.Config )
 	wm.Log = log.NewOWLogger(wm.Symbol())
 	return &wm
